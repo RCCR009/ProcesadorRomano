cda5bcd6dc4afe04bc93d6e9bda7563e5008cbc0
Rolando Conejo <rolandoconejo.cr@gmail.com>
2018-06-12 13:47:53 -0600

    Se implemeta primer test que no pasa la prueba

--- src/cr/co/cenfotec/logica/ProcesadorRomano.java
+++ src/cr/co/cenfotec/logica/ProcesadorRomano.java
@@ -2,4 +2,9 @@ package cr.co.cenfotec.logica;
 
 public class ProcesadorRomano {
 
+	public Object convierte(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- src/cr/co/cenfotec/logica/test/ProcesadorRomanoTest.java
+++ src/cr/co/cenfotec/logica/test/ProcesadorRomanoTest.java
@@ -2,13 +2,23 @@ package cr.co.cenfotec.logica.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import cr.co.cenfotec.logica.ProcesadorRomano;
+
 public class ProcesadorRomanoTest {
+	
+	ProcesadorRomano procesadorRomano;
+	
+	@Before
+	public void init() {
+		procesadorRomano = new ProcesadorRomano();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void StringVacio() {
+		assertEquals(0, procesadorRomano.convierte(""));
 	}
 
 }
